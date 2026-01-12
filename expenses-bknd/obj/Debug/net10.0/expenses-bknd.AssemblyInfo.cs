0214343ff3345b8f3d019f782c1d789ff3dd89cd
Yogesh Choudhary <39429365+yogeshrchoudhary@users.noreply.github.com>
2026-01-12 11:45:15 +0000

    delete front end

--- expenses-bknd/obj/Debug/net10.0/expenses-bknd.AssemblyInfo.cs
+++ expenses-bknd/obj/Debug/net10.0/expenses-bknd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("expenses-bknd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+add7dd6f97f10e1d9572c3e1f59e9b20f40388dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("expenses-bknd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("expenses-bknd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
